dafd4b98fbf36830e907d4d2b6fc2c4997f68976
SaranKumar <55644959+Sarankumar18@users.noreply.github.com>
2022-01-15 23:22:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
 <?php
-require 'dbconfig.php';
-require 'insert.php';
-require 'rsi.php';
-$json = file_get_contents('https://api.wazirx.com/sapi/v1/tickers/24hr');
+
+$json = file_get_contents('http://rsibot.epizy.com/');
  $data = json_decode($json);
 // $data = var_dump($obj);
- $sel_Currency = [];
-  for($i = 0;$i < sizeof($data);$i++){
-        if($data[$i]->quoteAsset == "inr"){
-            array_push($sel_Currency,$data[$i]);
-        }
-  }
 
-  //echo $sel_Currency[0]->at;
-  if(sizeof($sel_Currency) > 20)
-  {
-    insert($conn,$sel_Currency);
-    echo"Inserted";
-    cal_rsi($conn,$sel_Currency);
-  }
   
 ?>
